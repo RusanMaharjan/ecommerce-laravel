106d1532585729c2c4a6223b5a376d502ca0414e
RusanMaharjan <rusanmhz098@gmail.com>
2021-09-01 17:51:41 +0545

    commit

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -10,27 +10,27 @@ class CartComponent extends Component
 {
     //function for increasing quantity of product in cart
     public function increaseQuantity($rowId) {
-        $product = Cart::get($rowId);
+        $product = Cart::instance('cart')->get($rowId);
         $qty = $product->qty + 1;
-        Cart::update($rowId, $qty);
+        Cart::instance('cart')->update($rowId, $qty);
     }
 
     //function for decreasing quantity of product in cart
     public function decreaseQuantity($rowId) {
-        $product = Cart::get($rowId);
+        $product = Cart::instance('cart')->get($rowId);
         $qty = $product->qty - 1;
-        Cart::update($rowId, $qty);
+        Cart::instance('cart')->update($rowId, $qty);
     }
 
     //function for removing a quantity of product in cart
     public function destory($rowId) {
-        Cart::remove($rowId);
+        Cart::instance('cart')->remove($rowId);
         session()->flash('success_message','Item has been removed');   
     }
 
     //function for removing all quantity of product in cart at once
     public function deleteAll() {
-        Cart::destroy();  
+        Cart::instance('cart')->destroy();  
     }
 
     public function render()
--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -26,10 +26,14 @@ class ShopComponent extends Component
     }
 
     public function store($product_id, $product_name, $product_price) {
-        Cart::add($product_id, $product_name, 1, $product_price)
+        Cart::instance('cart')->add($product_id, $product_name, 1, $product_price)
             ->associate('App\Models\Product');
         session()->flash('success_message','Item added in cart');
         return redirect()->route('product.cart');
+    }
+
+    public function addToWishlist($product_id, $product_name, $product_price) {
+        Cart::instance('wishlist')->add($product_id, $product_name, $product_price)->associate('App\Models\Product');
     }
 
     use WithPagination;
